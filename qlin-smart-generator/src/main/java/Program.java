8fa1b6e5e36709d7932e0b177e4c78d6e6dfc499
tomfang <tomfang@126.com>
2017-08-30 10:49:31 +0800

    mybatis代码生成器优化

--- qlin-smart-generator/src/main/java/Program.java
+++ qlin-smart-generator/src/main/java/Program.java
@@ -8,21 +8,21 @@ import java.util.ArrayList;
 
 
 public class Program {
-	public static void main(String[] args) {
-		java.util.List<String> warnings = new ArrayList<String>();
-		boolean overwrite = true;
+    public static void main(String[] args) {
+        java.util.List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
 
-		File configFile = new File("F:/Project/project/smartsln-v1/qlin-smart/qlin-smart-generator/src/main/resources/generatorConfig.xml");
-		ConfigurationParser cp = new ConfigurationParser(warnings);
+        File configFile = new File("./src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
 
-		try {
-			Configuration config = cp.parseConfiguration(configFile);
-			DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-			MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-			myBatisGenerator.generate(null);
-			System.out.println("生成完成。。。");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+            System.out.println("生成完成。。。");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
